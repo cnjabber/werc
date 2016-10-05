3c578ba88a1a238b1141eb9e3e880864cb4a6c81
Iru Cai <mytbk920423@gmail.com>
2016-10-04 23:10:44 -0400

    add a fallback rendering notice

--- bin/contrib/pandoc_format.sh
+++ bin/contrib/pandoc_format.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ART="$(/bin/cat "$@" | /bin/sed 's/\(\[.*\](.*\)\.md)/\1)/g')"
+ART_FALLBACK="$(/bin/cat <<< "$ART"; echo -e '\n(fallback rendering with pandoc -f markdown_github)\n')"
 /usr/bin/pandoc -f markdown_mmd -t html5 <<< "$ART" \
-   || /usr/bin/pandoc -f markdown_github -t html5 $1 <<< "$ART"
+   || /usr/bin/pandoc -f markdown_github -t html5 $1 <<< "$ART_FALLBACK"
